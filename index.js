b122602bd15ed3988e084fac7bb983a85780eb62
Daniel Prieto <samudex@gmail.com>
2021-03-28 12:37:00 -0400

    temporary commit

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const btn = document.getElementById('add');
-btn.addEventListener('click', function () {
-    console.log('click!');
-})
+document.addEventListener('DOMContentLoaded', function () {
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const table = document.getElementById('table');
+    const btn = document.getElementById('add');
+
+    btn.onclick = function () {
+        console.log('Title', title.value);
+        console.log('Description', description.value);
+    }
+
+    function addTodo() {
+        if (title.value === '' || description.value === '') {
+            console.error('Title and description are required');
+        } else {
+            console.log('ok');
+        }
+
+    }
+});
